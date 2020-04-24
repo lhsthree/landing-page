58f3e70d3e91064b3907289f799611fa431f8419
Louis Steimel <lhsthree@gmail.com>
2020-04-24 15:50:25 -0500

    added section active states

--- js/app.js
+++ js/app.js
@@ -19,14 +19,11 @@
 */
 const sections = document.querySelectorAll('section');
 const navbar = document.querySelector('#navbar__list');
-
-
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
-
 
 
 /**
@@ -35,25 +32,41 @@ const navbar = document.querySelector('#navbar__list');
  *
 */
 
+
 // build the nav
     sections.forEach(e =>{
         const li = document.createElement('li')
         li.dataset.nav = `${e.id}`
-        li.className = `menu__link ${e.className}`
+        li.className = `menu__link`
         li.innerHTML = `${e.dataset.nav}`;
         navbar.appendChild(li)
-    })
+    });
 
 
 // Add class 'active' to section when near top of viewport
 
 
+    window.addEventListener('scroll', e =>{
+            sections.forEach( e=> {
+                const section = e.getBoundingClientRect();
+                const li = document.querySelector('li[data-nav="' + e.id + '"]');
+                if (section.top <= 150 && section.bottom >= 150){
+                    e.classList.add('your-active-class');
+                    li.classList.add('active');
+                } else {
+                     e.classList.remove('your-active-class');
+                      li.classList.remove('active');
+                }
+                })
+            })
+        
+    
+
 // Scroll to anchor ID using scrollTO event
 
     navbar.addEventListener('click', e =>{
-        document.getElementById( e.target.dataset.nav).scrollIntoView()
-    })
-
+        document.getElementById(e.target.dataset.nav).scrollIntoView()
+    });
 
 /**
  * End Main Functions
@@ -61,9 +74,12 @@ const navbar = document.querySelector('#navbar__list');
  *
 */
 
+
+
 // Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
+
 
